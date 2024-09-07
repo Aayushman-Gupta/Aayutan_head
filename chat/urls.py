574464cb984c309693fa518dade8e8874d6321d7
Aayushman-Gupta <aayushmangupta780@gmail.com>
2024-09-07 21:37:19 +0530

    created chat app and wrote consumer file

---
+++ chat/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+# from .views import chat_box
+from django.urls import path,include
+# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+urlpatterns = [
+    # path("<str:chat_box_name>/",chat_box, name="chat"),
+    
+]
