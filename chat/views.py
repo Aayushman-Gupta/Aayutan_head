9da1491ad6c1e2b7acc034543403015d5330cb3c
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-09-09 08:49:56 +0530

    chat function continue

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 import hashlib
 from rest_framework.decorators import api_view
-from django.views.decorators.http import require_POST
+from rest_framework.response import Response
+from rest_framework import status
 from django.http import JsonResponse
 from health_app.models import Patient
 from .models import ChatSession
@@ -13,13 +14,13 @@ def create_chat_session(request):
     print(username1)
     print(username2)
     if not username1 or not username2:
-        return JsonResponse({'error': 'user1 and user2 IDs are required.'}, status=400)
+        return Response(data={'error': 'user1 and user2 IDs are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
     try:
         user1 = Patient.objects.get(username=username1)
         user2 = Patient.objects.get(username=username2)
     except Patient.DoesNotExist:
-        return JsonResponse({'error': 'Invalid user1 or user2.'}, status=404)
+        return Response(data={'error': 'Invalid user1 or user2.'}, status=status.HTTP_404_NOT_FOUND)
 
     # Generate chat ID
     chat_id = generate_chat_id(user1, user2)
@@ -28,15 +29,17 @@ def create_chat_session(request):
     chat_session, created = ChatSession.objects.get_or_create(chat_id=chat_id)
 
     if created:
-        return JsonResponse({'message': 'Chat session created.', 'chat_id': chat_id}, status=201)
+        return Response(data={'message': 'Chat session created.', 'chat_id': chat_id}, status=status.HTTP_201_CREATED)
     else:
-        return JsonResponse({'message': 'Chat session already exists.', 'chat_id': chat_id}, status=200)
+        return Response(data={'message': 'Chat session already exists.', 'chat_id': chat_id}, status=status.HTTP_200_OK)
 
 
 def generate_chat_id(user1, user2):
     # Ensure consistent ordering to make it independent of who is the user1 or user2
     Patients = sorted([str(user1.username), str(user2.username)])
     combined = "".join(Patients)
+
+    # TODO : Ensure at most 95 characters
     # Create a hash of the combined string
     chat_id = hashlib.sha256(combined.encode()).hexdigest()
     return chat_id
