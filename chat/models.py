78dc3ea1b9464b219304632d49ffbf8f3c0484cf
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-09-10 06:44:08 +0530

    Jwt done plus partial code restructure

--- chat/models.py
+++ chat/models.py
@@ -6,8 +6,16 @@ class ChatSession(models.Model):
     chat_id = models.CharField(max_length=64, unique=True)
     created_time = models.DateTimeField(auto_now_add=True)
 
+    # user1 user2 denotes the users associated with a chat
+    user1 = models.ForeignKey(
+        Patient, related_name='user1', on_delete=models.CASCADE, null=True)
+    user2 = models.ForeignKey(
+        Patient, related_name='user2', on_delete=models.CASCADE, null=True)
+
+    # TODO : for group chat store users in form of list
+
     def __str__(self):
-        return self.chat_id
+        return f"{self.user1}-{self.user2}"
 
 
 class Message(models.Model):
