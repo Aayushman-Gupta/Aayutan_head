97d33331fd63ab5414e2f448f633b86d3909e8b5
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-09-10 22:12:12 +0530

    Added Standard Api response plus chat views like delete

--- Aayutan/settings.py
+++ Aayutan/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -97,6 +97,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME':timedelta(hours=24),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=5),
     'BLACKLIST_AFTER_ROTATION': True,
     'TOKEN_BLACKLIST_ENABLED': True,
 }
