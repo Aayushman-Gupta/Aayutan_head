8b55491de7f0771b6ec2819c001ffc3ca266a3f8
Dhruv251004 <118115413+Dhruv251004@users.noreply.github.com>
2024-09-14 14:58:27 +0530

    appointment views

--- appointment/serializers.py
+++ appointment/serializers.py
@@ -1,20 +1,33 @@
 from rest_framework import serializers
-from .models import Appointment,Day
+from .models import Appointment, Day, TakeAppointment
 from health_app.models import Doctor
 
 from health_app.serializers import DoctorSerializer
+
+
 class DaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Day
-        fields = ['id', 'name'] 
-        
+        fields = ['id', 'name']
+
 
 class AppointmentSerializer(serializers.ModelSerializer):
     doctor = DoctorSerializer(read_only=True)
-    doctor_id = serializers.PrimaryKeyRelatedField(queryset=Doctor.objects.all(), source='doctor', write_only=True)
+    doctor_id = serializers.PrimaryKeyRelatedField(
+        queryset=Doctor.objects.all(), source='doctor', write_only=True)
     appointment_days = DaySerializer(many=True, read_only=True)
-    appointment_days_ids = serializers.PrimaryKeyRelatedField(queryset=Day.objects.all(), many=True, source='appointment_days', write_only=True)
+    appointment_days_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Day.objects.all(), many=True, source='appointment_days', write_only=True)
 
     class Meta:
         model = Appointment
-        fields = ['id', 'doctor', 'doctor_id', 'appointment_days', 'appointment_days_ids', 'location', 'start_time', 'end_time', 'created_at']
+        fields = ['id', 'doctor', 'doctor_id', 'appointment_days',
+                  'appointment_days_ids', 'location', 'start_time', 'end_time', 'created_at']
+
+
+class TakeAppointmentSerializer(serializers.ModelSerializer):
+    appointment = AppointmentSerializer()
+
+    class Meta:
+        model = TakeAppointment
+        fields = ['appointment', 'phone_number', 'date']
